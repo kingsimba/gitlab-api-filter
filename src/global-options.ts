d1f386e9c45040fb641c4a6ee18d2ccf97f78a7a
kingsimba <kingsimba@mapbar.com>
2020-09-03 20:04:07 +0800

    use environment variable

--- src/global-options.ts
+++ src/global-options.ts
@@ -3,28 +3,36 @@ import deepExtend from "deep-extend";
 import stripJsonComments from "strip-json-comments";
 
 export class GlobalOptions {
-    public static configFileName = 'gitlab-api-filter.jsonc';
-    port = 8080;
-    url = "https://gitlab.example.com";
-    accessToken: string = '';
-    filters = [];
+    public errorMessage: string | undefined;
 
-    static instanceWithDefaultConfigFile(): GlobalOptions | undefined {
-        let o: GlobalOptions | undefined = new GlobalOptions();
+    public static configFileName = 'gitlab-api-filter.jsonc';
+    public port = 8080;
+    public url = "https://gitlab.example.com";
+    public filters: string[] = [];
+    accessToken: string = '';
+
+    static instanceWithDefaultConfigFile(): GlobalOptions {
+        const o: GlobalOptions = new GlobalOptions();
+        const fileName = GlobalOptions.configFileName;
         try {
             // read from file
-            let text = fs.readFileSync(GlobalOptions.configFileName, 'utf8');
+            if (!fs.existsSync(fileName)) {
+                throw new Error(`config file ${fileName} not exist`)
+            }
+            let text = fs.readFileSync(fileName, 'utf8');
             text = stripJsonComments(text);
             const config = JSON.parse(text);
             deepExtend(o, config);
 
             // read from environment variables
-            if (process.env.GITLAB_ACCESS_TOKEN) {
-                o.accessToken = process.env.GITLAB_ACCESS_TOKEN;
+            if (process.env.GITLAB_AF_ACCESS_TOKEN) {
+                o.accessToken = process.env.GITLAB_AF_ACCESS_TOKEN;
+            }
+            if (process.env.GITLAB_AF_URL) {
+                o.url = process.env.GITLAB_AF_URL;
             }
         } catch (error) {
-            o = undefined;
-            console.log(error);
+            o.errorMessage = error;
         }
 
         return o;
--- src/index.ts
+++ src/index.ts
@@ -2,30 +2,23 @@ import express from "express";
 import request from "request";
 import { globalOptions, GlobalOptions } from "./global-options";
 
-const PORT = 8080; // default port to listen
-const GITLAB = 'https://gitlab.navinfo.com';
-const PERSONAL_ACCESS_TOKEN = 'uWyWh3iXTD2guy2RyHLe';  // acquired from https://gitlab.example.com/profile/personal_access_tokens
-const FILTER = [
-    '/api/v4/projects',
-    '/api/v4/projects/:id/repository/branches',
-    '/api/v4/projects/:id/repository/tags']
-
 // Create Express instance
 export const app = express();
 
 app.use('/', async (req, res, next) => {
-    if (globalOptions == undefined) {
-        res.status(500).send({ message: `config file ${GlobalOptions.configFileName} not loaded correctly` });
+    if (globalOptions.errorMessage) {
+        res.status(500).send({ message: globalOptions.errorMessage });
+    } else {
+        next();
     }
-    next();
 });
 
 // Demonstrate how to read data from another server and return the result.
-app.get(FILTER, async (req, res) => {
+app.get(globalOptions.filters, async (req, res) => {
     const headers = {
-        'Private-Token': PERSONAL_ACCESS_TOKEN
+        'Private-Token': globalOptions.accessToken
     }
-    request(`${GITLAB}/${req.path}`, { json: true, headers }, (err, res1, body) => {
+    request(`${globalOptions.url}/${req.path}`, { json: true, headers }, (err, res1, body) => {
         if (res1) {
             res.send(res1.body);
         } else {
@@ -35,6 +28,6 @@ app.get(FILTER, async (req, res) => {
 });
 
 // start the Express server
-app.listen(PORT, () => {
-    console.log(`server started at http://localhost:${PORT}`);
+app.listen(globalOptions.port, () => {
+    console.log(`server started at http://localhost:${globalOptions.port}`);
 });
